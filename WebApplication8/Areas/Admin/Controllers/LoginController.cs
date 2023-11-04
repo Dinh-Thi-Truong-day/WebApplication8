541d0be9ba4a580d524e5fec1ab849a8dd683a43
truongdinhthi <example@gmail.com>
2023-11-04 21:51:49 +0700

    project

---
+++ WebApplication8/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication8.Areas.Models;
+
+namespace WebApplication8.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(WebApplication8.Areas.Models.User userModel)
+        {
+            using (LoginDataBaseEntities1 db = new LoginDataBaseEntities1())
+            {
+                var userDeatails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDeatails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["UserID"] = userDeatails.UserID;
+                    return RedirectToAction("index", "Home");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApplication8/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication8.Areas.Models;
+
+namespace WebApplication8.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(User userModel)
+        {
+            using (DbModels dbModels = new DbModels())
+            {
+                if (dbModels.Users.Any(x => x.UserName == userModel.UserName))
+                {
+                    ViewBag.DuplicateMessage = "Username already exist.";
+                    return View("AddOrEdit", userModel);
+                }
+
+                dbModels.Users.Add(userModel);
+                dbModels.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful";
+            return View("AddOrEdit", new User());
+        }
+    }
+}
